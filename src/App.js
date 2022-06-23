1bf848eef22d21e7135f2ce24944a87d33fafbed
zgulen <gulenzubeyir@gmail.com>
2022-06-23 21:51:39 +0300

    sass in react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import "./scss/App.scss"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
